d6efa04133ed1e71f394ef404a8c99fb9a6a1667
Jian Huang <jhuangsoftware@gmail.com>
2013-02-27 14:42:14 -0500

    ScoreTable .NET 2.0

---
+++ .NET Samples/ScoreTable/Published/ScoreTable.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ScoreTable.ascx.cs" Inherits="ScoreTable.ScoreTable" %>
+<form runat="server">
+<asp:GridView ID="TableGridView" runat="server">
+</asp:GridView>
+</form>
---
+++ .NET Samples/ScoreTable/ScoreTable/ScoreTable.ascx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+using System.Data;
+
+namespace ScoreTable
+{
+    public partial class ScoreTable : System.Web.UI.UserControl
+    {
+        private string _DataXmlPath;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.DataXmlPath) != true)
+            {
+                string FilePath = Server.MapPath(this.DataXmlPath);
+
+                DataSet DS = new DataSet();
+                DS.ReadXml(FilePath);
+
+                this.TableGridView.DataSource = DS.Tables[0];
+                this.TableGridView.DataBind();
+            }
+        }
+
+        public string DataXmlPath
+        {
+            get { return _DataXmlPath; }
+
+            set { _DataXmlPath = value; }
+        }
+    }
+}
